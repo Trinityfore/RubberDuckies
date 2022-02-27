03fb6c2e4297ea53a0aed3554224a8bfbdcc3f74
Trinity Fore <91577073+Trinityfore@users.noreply.github.com>
2022-02-27 11:54:30 -0600

    created superuser, admin not working

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,8 @@
 from dataclasses import field
-from msilib.schema import Class
 from django.forms import ModelForm
 from .models import Display
 
 class DisplayForm(ModelForm):
     class Meta:
         model = Display
-        fields = ('date', 'location')
+        fields = ('from_date','to_date', 'location')
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Duck, Display, Accessories
+from .models import Duck, Accessories
+from .forms import DisplayForm
 
 
 
@@ -18,7 +19,28 @@ def duckies_index(request):
 
 def duckies_detail(request, duck_id):
     duck = Duck.objects.get(id=duck_id)
-    return render(request, 'duckies/details.html', {'duck':duck})
+    display_form = DisplayForm()
+
+    available_accessories = Duck.objects.exclude(id__in = duck.accessories.all().values_list('id'))
+
+    return render(request, 'duckies/details.html', {
+        'duck': duck,
+        'display_form' : display_form,
+        'accessories' : available_accessories,
+    })
+
+def add_location(request, duck_id):
+    form = DisplayForm(request.POST)
+    if form.is_valid():
+        new_location = form.save(commit=False)
+        new_location.duck_id = duck_id
+        new_location.save()
+    return redirect ('detail', duck_id=duck_id)
+
+def assoc_access(request, duck_id, accessorie_id):
+    Duck.objects.get(id=duck_id).accessories.add(accessorie_id)
+    return redirect ('detail', duck_id=duck_id)
+
 
 class DuckCreate(CreateView): 
     model = Duck
@@ -33,3 +55,27 @@ class DuckUpdate(UpdateView):
 class DuckDelete(DeleteView):
     model = Duck
     success_url = '/cats/'
+
+class AccessorieCreate(CreateView):
+    model = Accessories
+    fields = ('name', 'color')
+
+
+class AccessorieUpdate(UpdateView):
+    model = Accessories
+    fields = ('name', 'color')
+
+
+class AccessorieDelete(DeleteView):
+    model = Accessories
+    success_url = '/accessories/'
+
+
+class AccessorieDetail(DetailView):
+    model = Accessories
+    template_name = 'accessories/detail.html'
+
+
+class AccessorieList(ListView):
+    model = Accessories
+    template_name = 'accessories/index.html'
