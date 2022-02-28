230164fdd7a0925f1e19da6cb658f4c1eb8a334b
Trinity Fore <91577073+Trinityfore@users.noreply.github.com>
2022-02-28 12:49:39 -0600

    adds accessorie template and urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
+from main_app.models import Accessories
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('duckies/', views.duckies_index, name='index'),
@@ -9,4 +11,12 @@ urlpatterns = {
     path('duckies/create/', views.DuckCreate.as_view(), name='ducks_create'),
     path('duckies/<int:pk/update/', views.DuckUpdate.as_view(), name='ducks_update'),
     path('duckies/<int:pk/delete/', views.DuckDelete.as_view(), name='ducks_delete'),
-}
+    path('duckies/<int:duck_id>/add_location/', views.add_location, name='add_location'),
+    path('accessories/', views.AccessorieList.as_view(), name = 'accessories_index'),
+    path('accessories/<int:pk>/', views.AccessorieDetail.as_view(), name = 'accessories_detail'),
+    path('accessories/create', views.AccessorieCreate.as_view(), name = 'accessories_create'),
+    path('accessories/<int:pk>/update/', views.AccessorieUpdate.as_view(), name = 'accessories_update'),
+    path('accessories/<int:pk>/delete/', views.AccessorieDelete.as_view(), name = 'accessories_delete'),
+    path('duckies/<int:duck_id>/assoc_access/<int:accessories_id>/', views.assoc_access, name='assoc_access'),
+
+]
